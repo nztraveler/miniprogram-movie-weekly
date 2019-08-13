e4741a81d419fa95339d18782020890b86e43a40
Francis <husihao1994@gmail.com>
2019-08-13 17:35:00 +0800

    feat: add back to current control  to movie weekly

--- pages/weekly/weekly.js
+++ pages/weekly/weekly.js
@@ -9,28 +9,45 @@ Page({
       name: '教父',
       comment: '最精彩的电影。。。',
       image: '/images/my_avatar.jpg',
-      isHighlyRecommended:false
-    },{
-        name: '教父2',
-        comment: '最精彩的电影。。。',
-        image: '/images/my_avatar.jpg',
-        isHighlyRecommended: false
-    },{
-        name: '教父3',
-        comment: '最精彩的电影。。。',
-        image: '/images/my_avatar.jpg',
-        isHighlyRecommended: false
+      isHighlyRecommended: false
+    }, {
+      name: '教父2',
+      comment: '最精彩的电影。。。',
+      image: '/images/my_avatar.jpg',
+      isHighlyRecommended: false
+    }, {
+      name: '教父3',
+      comment: '最精彩的电影。。。',
+      image: '/images/my_avatar.jpg',
+      isHighlyRecommended: false
     }],
-    count:213,
-    score:55
+    count: 213,
+    score: 55,
+    currentIndex: 0
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function(options) {
-
+    this.setData({
+      currentIndex: this.data.thisWeekMovieList.length - 1
+    })
   },
+
+  onBackTap: function(e){
+    console.log(e)
+    this.setData({
+      currentIndex: this.data.thisWeekMovieList.length - 1
+    })
+  },
+
+  // onChange: function (event){
+  //   console.log(event)
+  //   this.setData({
+  //     currentIndex: current
+  //   })
+  // },
 
   /**
    * Lifecycle function--Called when page is initially rendered
